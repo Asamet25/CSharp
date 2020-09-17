625874538de55905d639b15bc4117697b07364ae
Asamet25 <68554867+Asamet25@users.noreply.github.com>
2020-09-17 20:19:38 +0300

    Add files via upload

---
+++ üslü-sayi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Taban Sayıyı Giriniz");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Üsü Giriniz");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            double uslusayi = Math.Pow(number1,number2);
+            Console.WriteLine(number1 + " üzeri " + number2 + " = "+ uslusayi);    
+        }
+    }
+}
